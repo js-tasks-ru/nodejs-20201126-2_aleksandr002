67a2f802e1e1d63336e867c1c581fcc40ae60d25
Alexander Tarasov <sovtara@gmail.com>
2020-12-14 11:13:16 +0300

    03-streams 01-limit-size-stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,21 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #limit = 0;
+  #streamSize = 0;
+  
   constructor(options) {
     super(options);
+    this.#limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.#streamSize += chunk.length;
+    if ( this.#streamSize > this.#limit ) {
+      return this.destroy(new LimitExceededError());
+    }
+    this.push(chunk);
+    callback();
   }
 }
 
