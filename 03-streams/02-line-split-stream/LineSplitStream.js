3b04bb71db4997726f3bf3f6c837c6d945062b19
Alexander Tarasov <sovtara@gmail.com>
2020-12-15 17:12:17 +0300

    03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,14 +2,24 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+
+  #complete = '';
+
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    this.#complete += chunk;
+    callback();
   }
 
   _flush(callback) {
+    const parts = this.#complete.split(`${os.EOL}`);
+    parts.forEach(part => {
+      this.push(part);
+    });
+    callback();
   }
 }
 
